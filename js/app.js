5cdc5d4bee62ea44b073d0d13cc7b448408f9923
Sergio Diaz <oigres.zaid@gmail.com>
2019-08-06 20:13:57 -0500

    Se crean  ejemplos de modo stricto en archivo js/app.js, Variable no declarada, Uso de palabra resercada

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// Se activa modo stricto ('use strict')
+'use strict'
+
 //alert("Hola Mundo desde un archivo externo")
 
 console.log("Hola Consola")
+
+// Variable no declarada
+var x;
+x = "3.1416";
+
+// Viable con palabra reservada
+
+var publicData = "Hola"
